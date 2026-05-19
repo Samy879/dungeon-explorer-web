39a4151b8be1372659e53899e8e9f6bd74144792
Samy <99542386+Samy879@users.noreply.github.com>
2026-05-19 14:05:29 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -71,7 +71,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpy6sjppp3.js
+// include: /tmp/tmpogbfiue9.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -199,25 +199,25 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
     }
 
     }
-    loadPackage({"files": [{"filename": "/donjon16.txt", "start": 0, "end": 70}, {"filename": "/donjon32.txt", "start": 70, "end": 926}, {"filename": "/donjon64.txt", "start": 926, "end": 1148}, {"filename": "/donjon_epic.txt", "start": 1148, "end": 2884}], "remote_package_size": 2884});
+    loadPackage({"files": [{"filename": "/donjon16.txt", "start": 0, "end": 70}, {"filename": "/donjon32.txt", "start": 70, "end": 926}, {"filename": "/donjon64.txt", "start": 926, "end": 1148}, {"filename": "/donjon_epic.txt", "start": 1148, "end": 2884}, {"filename": "/donjon_remy.txt", "start": 2884, "end": 6589}], "remote_package_size": 6589});
 
   })();
 
-// end include: /tmp/tmpy6sjppp3.js
-// include: /tmp/tmpibvyi2eq.js
+// end include: /tmp/tmpogbfiue9.js
+// include: /tmp/tmpajc56ory.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpibvyi2eq.js
-// include: /tmp/tmp1np2wuig.js
+  // end include: /tmp/tmpajc56ory.js
+// include: /tmp/tmpzn10hf1n.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmp1np2wuig.js
+  // end include: /tmp/tmpzn10hf1n.js
 
 
 var arguments_ = [];
